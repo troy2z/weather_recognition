2031c7bd84fa8d55c68bd96654469b6f51c10757
troy2z <153445149+troy2z@users.noreply.github.com>
2024-03-14 16:55:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ def main():
 
 
 if __name__ == '__main__':
-
+    os.environ['TORCH_HOME'] = Constant.TORCH_HOME
     os.makedirs(config["model_dir"], exist_ok=True)
     os.makedirs(config["log_dir"], exist_ok=True)
     os.environ['CUDA_VISIBLE_DEVICES'] = config["CUDA_VISIBLE_DEVICES"]
